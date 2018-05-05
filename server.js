f5fc59fc34d4aa775ef0a8a8a0638a344a5e5343
Christopher Gregory Zoghby <cgzoghby@gmail.com>
2018-05-05 13:40:44 -0400

    Add A Tiny Bit More CSS

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ var app = express();
 
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
+//This second app.use allows me to actually pull the stupid burger picture.
+app.use(express.static("public/assets/img"));
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
